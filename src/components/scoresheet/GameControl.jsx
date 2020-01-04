6d8d67ecc1ec2f979ad122a0c17d1f1e1cc5f3dc
Spencer Lyons <sjl8r8@gmail.com>
2020-01-04 10:58:42 -0600

    first frame completed, frame markings working

--- src/components/scoresheet/GameControl.jsx
+++ src/components/scoresheet/GameControl.jsx
@@ -3,7 +3,7 @@ import Scoreframes from "./Scoreframes";
 import Pins from "./Pins";
 
 class Frame {
-    constructor(name, ballOne, ballTwo, frameScore, totalScore, frameComplete, scoreComplete, ballOneComplete){
+    constructor(name, ballOne, ballTwo, frameScore, totalScore, frameComplete, scoreComplete, ballOneComplete, ballTwoComplete){
         this.name = name;
         this.ballOne = ballOne;
         this.ballTwo = ballTwo;
@@ -11,37 +11,38 @@ class Frame {
         this.totalScore = totalScore;
         this.frameComplete = frameComplete;
         this.scoreComplete = scoreComplete;
-        this.ballOneComplete = ballOneComplete
+        this.ballOneComplete = ballOneComplete;
+        this.ballTwoComplete = ballTwoComplete;
     }
 };
 
 
-const frame1 = new Frame('Frame One', '', '', 0, 0, false, false, false);
+const frame1 = new Frame('Frame One', '', '', 0, 0, false, false, false, false);
 
-function rackPins(value) {
-    let pinsLeft = 10 - value;
-    switch(pinsLeft) {
-        case 9:
-            console.log('1 pin hit');
-            break;
-        case 8: 
-            console.log('2 pins hit');
-    }
-}
+// function rackPins(value) {
+//     let pinsLeft = 10 - value;
+//     switch(pinsLeft) {
+//         case 9:
+//             console.log('1 pin hit');
+//             break;
+//         case 8: 
+//             console.log('2 pins hit');
+//     }
+// }
 
 const pushPinsToFrame = (frameArray) => {
-    //Is frame 1 complete?
-    //Yes - go to frame 2
-    //No - figure out first ball or second ball... tally scores... complete the frame
     if (!frame1.frameComplete) {
         if (!frame1.ballOneComplete) {
-            //do something for ball 1
             frame1.ballOne = frameArray.shift();
-            rackPins(frame1.ballOne)
+            // rackPins(frame1.ballOne)
             frame1.ballOneComplete = true;
+            if (frame1.ballOne === 10) {
+                frame1.frameComplete = true;
+            }
+            
         } else {
-            //do something for ball 2
             frame1.ballTwo = frameArray.shift();
+            frame1.ballTwoComplete = true;
             frame1.frameComplete = true;
         }
     }
@@ -51,8 +52,7 @@ const pushPinsToFrame = (frameArray) => {
 function GameControl() {
     const [frameOne, setFrameOne] = useState({
         frame1a: '',
-        frame1b: '',
-       
+        frame1b: '',       
     });
 
     
@@ -64,16 +64,30 @@ function GameControl() {
         console.log(frameArray)  
         console.log(frame1); 
         setFrameOne(prevValue => {
-            if (frame1.ballOneComplete && !frame1.frameComplete) {
-                return {
-                    frame1a: frame1.ballOne,
-                    frame1b: prevValue.frame1b
-                }
+            if (frame1.ballOneComplete && !frame1.ballTwoComplete) {
+                if(frame1.ballOne === 10) {
+                    return {
+                        frame1a: prevValue.frame1a,
+                        frame1b: 'X'
+                    }
+                } else {
+                    return {
+                        frame1a: frame1.ballOne,
+                        frame1b: prevValue.frame1b
+                    }
+            }
             } else if (frame1.ballOneComplete && frame1.frameComplete) {
-                return {
-                    frame1a: prevValue.frame1a,
-                    frame1b: frame1.ballTwo
-                }
+                if (frame1.ballOne + frame1.ballTwo === 10) {
+                    return {
+                        frame1a: prevValue.frame1a,
+                        frame1b: '/'
+                    }
+                } else {
+                    return {
+                        frame1a: prevValue.frame1a,
+                        frame1b: frame1.ballTwo
+                    }
+            }
             }
         })   
       }
