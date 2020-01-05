fac6efbe30962797d92a5daab98b3159fd70e8dc
Spencer Lyons <sjl8r8@gmail.com>
2020-01-04 20:34:35 -0600

    rack pins now working

--- src/components/scoresheet/GameControl.jsx
+++ src/components/scoresheet/GameControl.jsx
@@ -26,62 +26,68 @@ class Frame {
   }
 }
 let currentFrame = 1;
+let isRollOne = true;
 const frame1 = new Frame("Frame One", "", "", 0, 0, false, false, false, false);
 const frame2 = new Frame("Frame Two", "", "", 0, 0, false, false, false, false);
 
+// function rackPins(value) {
+//     let pinsLeft = 10 - value;
+//     if (pinsLeft === 9) {
+//       setPinTen(true);
 
-function rackPins(value) {
-    let pinsLeft = 10 - value;
-    if (pinsLeft === 1) {
-      console.log('daMN')
-    }
-}
+//     }
+// }
 
 const pushPinsToFrame = frameArray => {
-    if (!frame1.frameComplete && currentFrame ===1) {
-      if (!frame1.ballOneComplete) {
-        frame1.ballOne = frameArray.shift();
-        rackPins(frame1.ballOne)
-        frame1.ballOneComplete = true;
-        if (frame1.ballOne === 10) {
-          frame1.frameComplete = true;
-          currentFrame++
-        }
-      } else {
-        frame1.ballTwo = frameArray.shift();
-        frame1.ballTwoComplete = true;
-        frame1.frameComplete = true; 
-        currentFrame++  
-        
+  if (!frame1.frameComplete && currentFrame === 1) {
+    if (!frame1.ballOneComplete) {
+      frame1.ballOne = frameArray.shift();
+      // rackPins(frame1.ballOne)
+      frame1.ballOneComplete = true;
+      if (frame1.ballOne === 10) {
+        frame1.frameComplete = true;
+        currentFrame++;
       }
-    
-  } else if (!frame2.frameComplete && currentFrame === 2) {
-      if (!frame2.ballOneComplete) {
-        frame2.ballOne = frameArray.shift();
-        // rackPins(frame1.ballOne)
-        frame2.ballOneComplete = true;
-        if (frame2.ballOne === 10) {
-          frame2.frameComplete = true;
-          currentFrame++
-        }
-      } else {
-        frame2.ballTwo = frameArray.shift();
-        frame2.ballTwoComplete = true;
-        frame2.frameComplete = true;
-        currentFrame++
-      }
+    } else {
+      frame1.ballTwo = frameArray.shift();
+      frame1.ballTwoComplete = true;
+      frame1.frameComplete = true;
+      currentFrame++;
     }
+  } else if (!frame2.frameComplete && currentFrame === 2) {
+    if (!frame2.ballOneComplete) {
+      frame2.ballOne = frameArray.shift();
+      // rackPins(frame1.ballOne)
+      frame2.ballOneComplete = true;
+      if (frame2.ballOne === 10) {
+        frame2.frameComplete = true;
+        currentFrame++;
+      }
+    } else {
+      frame2.ballTwo = frameArray.shift();
+      frame2.ballTwoComplete = true;
+      frame2.frameComplete = true;
+      currentFrame++;
+    }
+  }
 };
 
 function GameControl() {
   //useState for Pins. Should rerack after ball one unless it's a strike.
-  // const [pinsUp, setPinsUp] = useState({
-
-  // })
+  const [pinTen, setPinTen] = useState(false);
+  const [pinNine, setPinNine] = useState(false);
+  const [pinEight, setPinEight] = useState(false);
+  const [pinSeven, setPinSeven] = useState(false);
+  const [pinSix, setPinSix] = useState(false);
+  const [pinFive, setPinFive] = useState(false);
+  const [pinFour, setPinFour] = useState(false);
+  const [pinThree, setPinThree] = useState(false);
+  const [pinTwo, setPinTwo] = useState(false);
+  // const [pinOne, setPinOne] = useState(false);
 
   const [frameOne, setFrameOne] = useState({
     frame1a: "",
-    frame1b: "",
+    frame1b: ""
   });
   const [frameTwo, setFrameTwo] = useState({
     frame2a: "",
@@ -93,6 +99,88 @@ function GameControl() {
     let value = event.target.value;
     frameArray.push(Number(value));
     pushPinsToFrame(frameArray);
+
+    //Rack the Pins
+    if (isRollOne) {
+      let pinsLeft = 10 - value;
+      if (pinsLeft === 9) {
+        setPinTen(true);
+        isRollOne=false;
+      } else if (pinsLeft === 8) {
+        setPinTen(true);
+        setPinNine(true);
+        isRollOne=false;
+      } else if (pinsLeft === 7) {
+        setPinTen(true);
+        setPinNine(true);
+        setPinEight(true);
+        isRollOne=false;
+      } else if (pinsLeft === 6) {
+        setPinTen(true);
+        setPinNine(true);
+        setPinEight(true);
+        setPinSeven(true);
+        isRollOne=false;
+      } else if (pinsLeft === 5) {
+        setPinTen(true);
+        setPinNine(true);
+        setPinEight(true);
+        setPinSeven(true);
+        setPinSix(true);
+        isRollOne=false;
+      } else if (pinsLeft === 4) {
+        setPinTen(true);
+        setPinNine(true);
+        setPinEight(true);
+        setPinSeven(true);
+        setPinSix(true);
+        setPinFive(true);
+        isRollOne=false;
+      } else if (pinsLeft === 3) {
+        setPinTen(true);
+        setPinNine(true);
+        setPinEight(true);
+        setPinSeven(true);
+        setPinSix(true);
+        setPinFive(true);
+        setPinFour(true);
+        isRollOne=false;
+      } else if (pinsLeft === 2) {
+        setPinTen(true);
+        setPinNine(true);
+        setPinEight(true);
+        setPinSeven(true);
+        setPinSix(true);
+        setPinFive(true);
+        setPinFour(true);
+        setPinThree(true);
+        isRollOne=false;
+      } else if (pinsLeft === 1) {
+        setPinTen(true);
+        setPinNine(true);
+        setPinEight(true);
+        setPinSeven(true);
+        setPinSix(true);
+        setPinFive(true);
+        setPinFour(true);
+        setPinThree(true);
+        setPinTwo(true);
+        isRollOne=false;
+      }
+    } else {
+        setPinTen(false);
+        setPinNine(false);
+        setPinEight(false);
+        setPinSeven(false);
+        setPinSix(false);
+        setPinFive(false);
+        setPinFour(false);
+        setPinThree(false);
+        setPinTwo(false);
+        isRollOne=true;
+    }
+
+    //Udate the frame markings
     setFrameOne(prevValue => {
       if (frame1.ballOneComplete && !frame1.ballTwoComplete) {
         if (frame1.ballOne === 10) {
@@ -149,13 +237,25 @@ function GameControl() {
         }
       });
     }
-    console.log(frame1)
+    console.log(frame1);
     console.log(frame2);
   }
 
   return (
     <div>
-      <Pins pinsKnocked={handlePinsKnocked} />
+      <Pins
+        pinsKnocked={handlePinsKnocked}
+        tenPinDown={pinTen}
+        ninePinDown={pinNine}
+        eightPinDown={pinEight}
+        sevenPinDown={pinSeven}
+        sixPinDown={pinSix}
+        fivePinDown={pinFive}
+        fourPinDown={pinFour}
+        threePinDown={pinThree}
+        twoPinDown={pinTwo}
+        // onePinDown={pinOne}
+      />
       <Scoreframes
         frame1a={frameOne.frame1a}
         frame1b={frameOne.frame1b}
--- src/components/scoresheet/Pins.jsx
+++ src/components/scoresheet/Pins.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
+//style={props.state}
 function Pins(props){
     return <div className="container pins">
     <div className="row">
       <div className="col-lg-12 pinsBtn">
-        <button onClick={props.pinsKnocked} className="button" id="Zero" value={0}>0</button>
-        <button onClick={props.pinsKnocked} className="button" id="One" value={1}>1</button>
-        <button onClick={props.pinsKnocked} className="button" id="Two" value={2}>2</button>
-        <button onClick={props.pinsKnocked} className="button" id="Three" value={3}>3</button>
-        <button onClick={props.pinsKnocked} className="button" id="Four" value={4}>4</button>
-        <button onClick={props.pinsKnocked} className="button" id="Five" value={5}>5</button>
-        <button onClick={props.pinsKnocked} className="button" id="Six" value={6}>6</button>
-        <button onClick={props.pinsKnocked} className="button" id="Seven" value={7}>7</button>
-        <button onClick={props.pinsKnocked} className="button" id="Eight" value={8}>8</button>
-        <button onClick={props.pinsKnocked} className="button" id="Nine" value={9}>9</button>
-        <button onClick={props.pinsKnocked} className="button" id="Ten" value={10}>10</button>
+        <button onClick={props.pinsKnocked} style={{display: props.zeroPinDown ? 'none' : 'inline'}}className="button" id="Zero" value={0}>0</button>
+        <button onClick={props.pinsKnocked} style={{display: props.OnePinDown ? 'none' : 'inline'}}className="button" id="One" value={1}>1</button>
+        <button onClick={props.pinsKnocked} style={{display: props.twoPinDown ? 'none' : 'inline'}}className="button" id="Two" value={2}>2</button>
+        <button onClick={props.pinsKnocked} style={{display: props.threePinDown ? 'none' : 'inline'}}className="button" id="Three" value={3}>3</button>
+        <button onClick={props.pinsKnocked} style={{display: props.fourPinDown ? 'none' : 'inline'}}className="button" id="Four" value={4}>4</button>
+        <button onClick={props.pinsKnocked} style={{display: props.fivePinDown ? 'none' : 'inline'}}className="button" id="Five" value={5}>5</button>
+        <button onClick={props.pinsKnocked} style={{display: props.sixPinDown ? 'none' : 'inline'}}className="button" id="Six" value={6}>6</button>
+        <button onClick={props.pinsKnocked} style={{display: props.sevenPinDown ? 'none' : 'inline'}}className="button" id="Seven" value={7}>7</button>
+        <button onClick={props.pinsKnocked} style={{display: props.eightPinDown ? 'none' : 'inline'}}className="button" id="Eight" value={8}>8</button>
+        <button onClick={props.pinsKnocked} style={{display: props.ninePinDown ? 'none' : 'inline'}}className="button" id="Nine" value={9}>9</button>
+        <button onClick={props.pinsKnocked} style={{display: props.tenPinDown ? 'none' : 'inline'}}className="button" id="Ten" value={10}>10</button>
       </div>
     </div>
   </div>
